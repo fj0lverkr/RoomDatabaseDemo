3e339dac30b185dba7e534a4016e26e0e9b46205
nils <nils.nahooy@live.com>
2022-09-23 15:26:53 +0200

    Setup of Adapter for RecyclerView

---
+++ app/src/main/java/com/nilsnahooy/roomdatabasedemo/ItemAdapter.kt
@@ -0,0 +1,51 @@
+package com.nilsnahooy.roomdatabasedemo
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getColor
+import androidx.recyclerview.widget.RecyclerView
+import com.nilsnahooy.roomdatabasedemo.databinding.ItemRowBinding
+
+class ItemAdapter(private val items: ArrayList<EmployeeEntity>,
+                  private val updateListener:(id:Int) -> Unit,
+                  private val deleteListener:(id:Int) -> Unit)
+    : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+
+    class ViewHolder(b: ItemRowBinding): RecyclerView.ViewHolder(b.root){
+        val clMain = b.clMain
+        val tvName = b.tvName
+        val tvMail = b.tvMail
+        val ivEdit = b.ivEdit
+        val ivDelete = b.ivDelete
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemRowBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val c =  holder.itemView.context
+        val item = items[position]
+        holder.tvName.text = item.name
+        holder.tvMail.text = item.email
+        holder.clMain.setBackgroundColor(
+            if(position%2==0){
+                getColor(c, R.color.white)
+            }else{
+                getColor(c,com.google.android.material.R.color.material_dynamic_neutral80)
+        })
+
+        holder.ivEdit.setOnClickListener {
+            updateListener.invoke(item.id)
+        }
+
+        holder.ivDelete.setOnClickListener {
+            deleteListener.invoke(item.id)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
